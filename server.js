f89d7749c7fbc914b4d178ced889ebc47fa1b7ef
Karin Ochayon <karinoch070@gmail.com>
2023-07-23 21:46:36 +0300

    Working on the socket.io

--- server.js
+++ server.js
@@ -2,20 +2,19 @@ const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
 const cors = require("cors");
-const { MongoClient } = require("mongodb");
 const mongoose = require("mongoose"); // Import mongoose
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-// const server = http.createServer(app);
-// const io = new Server(server, {
-//   cors: {
-//     origin: "http://localhost:3000",
-//     methods: ["GET", "POST"],
-//   },
-// });
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
 
 mongoose.connect("mongodb://localhost:27017/codingMentorshipDB", {
   useNewUrlParser: true,
@@ -37,6 +36,24 @@ const codeBlockSchema = new mongoose.Schema({
 // Create the code block model
 const CodeBlock = mongoose.model("CodeBlock", codeBlockSchema);
 
+io.on("connection", (socket) => {
+  console.log("A user connected");
+
+  socket.on("codeBlockChange", async (data) => {
+    try {
+      const { title, code } = data;
+      const codeBlock = await CodeBlock.findOneAndUpdate(
+        { title },
+        { code },
+        { new: true }
+      ).exec();
+      socket.broadcast.emit("codeBlockUpdated", codeBlock);
+    } catch (error) {
+      console.error("Error updating code block:", error);
+    }
+  });
+});
+
 // API route to get code block by title
 app.get("/codeblockpage/:title", async (req, res) => {
   const { title } = req.params;
@@ -49,6 +66,7 @@ app.get("/codeblockpage/:title", async (req, res) => {
   }
 });
 
-app.listen(3001, function () {
+
+server.listen(3001, function () {
   console.log("Yay! Server is running");
 });
