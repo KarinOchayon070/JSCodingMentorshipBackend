8c2b5e0f4baedeb5bffd5f82319a950cac6203fa
Karin Ochayon <karinoch070@gmail.com>
2023-07-25 17:18:34 +0300

    Edit :)

--- codeBlockModel.js
+++ codeBlockModel.js
@@ -1,20 +1,20 @@
 const mongoose = require("mongoose");
 
 const codeBlockSchema = new mongoose.Schema({
-    title: {
-      type: String,
-      required: true,
-    },
-    code: {
-      type: String,
-      required: true,
-    },
-    solution: {
-      type: String,
-      required: true,
-    },
-  });
-  
-  const CodeBlockModel = mongoose.model("CodeBlock", codeBlockSchema)
-  
-  module.exports = CodeBlockModel;
+  title: {
+    type: String,
+    required: true,
+  },
+  code: {
+    type: String,
+    required: true,
+  },
+  solution: {
+    type: String,
+    required: true,
+  },
+});
+
+const CodeBlockModel = mongoose.model("CodeBlock", codeBlockSchema);
+
+module.exports = CodeBlockModel;
